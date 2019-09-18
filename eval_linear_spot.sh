6409c1acd8264698ff7ddb181f3f20a10cd4dc65
rhodricusack <rhodricusack@gmail.com>
2019-09-18 10:17:59 +0100

    Set up linearclass on spot

---
+++ eval_linear_spot.sh
@@ -0,0 +1,28 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+#!/bin/bash
+
+DATA="/imagenet"
+
+PYTHON="/home/ubuntu/anaconda3/envs/pytorch_p36/bin/python"
+
+CHECKPOINTBUCKET="neurana-imaging"
+CHECKPOINTPATH="rhodricusack/alexnet/checkpoints"
+LINEARCLASSBUCKET="neurana-imaging"
+LINEARCLASSPATH="rhodricusack/alexnet/linearclass"
+
+SQSURL="https://sqs.eu-west-1.amazonaws.com/807820536621/deepcluster-linearclass.fifo"
+EXP="${HOME}/linearclass"
+echo "${EXP}"
+
+# This will only work for NVIDIA GPUs
+NGPUS=`lspci|grep 'NVIDIA'| wc -l`
+
+for ((tp=0;tp<NGPUS;tp++)); do 
+    mkdir -p ${EXP}_${tp}
+    (CUDA_VISIBLE_DEVICES=${tp}; ${PYTHON}  eval_linear_spot.py --data ${DATA} --epochs 2 --lr 0.01 --wd -7 --verbose --exp ${EXP}_${tp} --workers 8  --checkpointbucket ${CHECKPOINTBUCKET} --checkpointpath ${CHECKPOINTPATH} --sqsurl ${SQSURL} --linearclassbucket ${LINEARCLASSBUCKET} --linearclasspath ${LINEARCLASSPATH}) &
+done
---
+++ fill_sqs_queue_for_spot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+LINEARCLASSBUCKET="neurana-imaging"
+LINEARCLASSPATH="rhodricusack/alexnet/linearclass/"
+
+CHECKPOINTBUCKET="neurana-imaging"
+CHECKPOINTPATH="rhodricusack/alexnet/checkpoints"
+
+echo "Fill SQS with requests, where checkpoint exists but linearclass does not"
+for ((tp=1;tp<10;tp++)); do
+    getcheckpoint=$(aws s3 ls "s3://${CHECKPOINTBUCKET}/${CHECKPOINTPATH}/checkpoint_${tp}.pth.tar")
+    if [ ! -z "$getcheckpoint" ]; then
+        echo "Found checkpoint file $getcheckpoint"
+        for ((conv=5;conv>0; conv--)); do
+            gotfile=$(aws s3 ls "s3://${LINEARCLASSBUCKET}/${LINEARCLASSPATH}linearclass_time_${tp}_conv_${conv}")
+            if [ -z "$gotfile" ]; then
+                    echo "Nothing found for time $tp conv $conv";
+                    aws sqs send-message --queue-url "https://sqs.eu-west-1.amazonaws.com/807820536621/deepcluster-linearclass.fifo" \
+                        --message-body "{\"epoch\":$tp,\"conv\":$conv}" \
+                        --message-group-id "standard4"
+            fi
+        done
+    fi
+done
+         
