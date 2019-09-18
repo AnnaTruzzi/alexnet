b1a2f823f3f423dbf55b3b7cbd8d6969c7c06af7
Rhodri Cusack <rhodri.cusack@gmail.com>
2019-09-18 09:51:28 +0000

    Fixed loading of checkpoint

--- util.py
+++ util.py
@@ -20,22 +20,24 @@ def load_model(path):
         print("=> loading checkpoint '{}'".format(path))
         checkpoint = torch.load(path)
 
+        # deal with a dataparallel table
+        def rename_key(key):
+            if not 'module' in key:
+                return key
+            return ''.join(key.split('module.'))
+
+        checkpoint['state_dict'] = {rename_key(key): val
+                                    for key, val
+                                    in checkpoint['state_dict'].items()}
+
+        print(checkpoint['state_dict'].keys())
+
         # size of the top layer
         N = checkpoint['state_dict']['top_layer.bias'].size()
 
         # build skeleton of the model
         sob = 'sobel.0.weight' in checkpoint['state_dict'].keys()
         model = models.__dict__[checkpoint['arch']](sobel=sob, out=int(N[0]))
-
-        # deal with a dataparallel table
-        def rename_key(key):
-            if not 'module' in key:
-                return key
-            return ''.join(key.split('.module'))
-
-        checkpoint['state_dict'] = {rename_key(key): val
-                                    for key, val
-                                    in checkpoint['state_dict'].items()}
 
         # load weights
         model.load_state_dict(checkpoint['state_dict'])
